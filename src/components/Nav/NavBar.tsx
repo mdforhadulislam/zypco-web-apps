0c491bc747e3bef34ba2eeb1e8054ffa6448a9f2
tawhidul islam <mdforhadul44@gmail.com>
2025-08-01 02:48:06 +0600

    first commit

---
+++ src/components/Nav/NavBar.tsx
@@ -0,0 +1,30 @@
+import Logo from '@/utilities/Logo'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <section className='w-full h-auto'>
+        
+<div className='w-full h-auto'>
+
+    <div>
+
+        <Logo  />
+
+
+
+
+
+    </div>
+
+
+
+
+
+</div>
+
+    </section>
+  )
+}
+
+export default NavBar
---
+++ src/utilities/Logo.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface LogoProps {
+  isFooter?: boolean;
+  width?: number;
+  height?: number;
+}
+
+const Logo: React.FC<LogoProps> = ({isFooter = false, width, height}) => {
+  return (
+    <div>
+        {isFooter ? 
+            <Image
+            src="/footer-logo.jpg"
+            alt="Zypco Logo"
+            width={width}
+            height={height}
+            className="object-contain"
+            priority 
+        />:<Image
+        src="/logo.jpg"
+        alt="Zypco Logo"
+        width={100}
+        height={100}
+        className="object-contain w-[60px] h-[60px]"
+        priority 
+    />}
+        
+
+
+    </div>
+  )
+}
+
+export default Logo
