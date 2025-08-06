e9340e818169c6936eae451657e9d3eb2ec4ff6e
FOrhadul Islam <mdforhadul44@gmail.com>
2025-08-06 20:32:10 +0600

    addd alllll

--- src/components/Home/HeroHomeSection.tsx
+++ src/components/Home/HeroHomeSection.tsx
@@ -1,14 +1,39 @@
+import { Package } from 'lucide-react'
 import React from 'react'
 
 const HeroHomeSection = () => {
   return (
     <div className='w-full h-auto bg-1'>
-        <div className='container m-auto md:h-[90vh] '>
+        <div className='container m-auto md:h-[40vh] p-4'>
 
-        <div className='w-full h-auto pt-24'>
+        <div className='w-full h-auto pt-16 pb-5'>
 
-            <h1 className='text-5xl font-extrabold text-white'>ZYPCO EXPRESS</h1>
-            <h3 className='text-xl font-semibold text-white '>ZIP IT SHIP IT ZYPCO</h3>
+            <h1 className='text-4xl sm:text-5xl  font-extrabold text-white'>ZYPCO COURIER SOLUTATIONS</h1>
+            <h3 className='text-base sm:text-xl md:text-3xl font-semibold text-white '>ZIP IT SHIP IT ZYPCO</h3>
+        </div>
+
+        <div className='w-full h-auto grid grid-cols-2 grid-rows-2 justify-between'>
+          <div className=' bg-white rounded-2xl p-4 shadow-2xl'>
+            <div className='w-full h-auto flex justify-center align-middle items-center'>
+              <div className='w-full h-auto bg-red-600 p-2 rounded-2xl'>
+                <Package  />
+              </div>
+
+
+
+
+            </div>
+
+
+          </div>
+
+
+
+
+
+
+
+<div></div>
         </div>
 
 
