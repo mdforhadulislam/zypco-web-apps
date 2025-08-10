796f03e6a5525170f850879fd0bdc8ee72ec1bb6
FOrhadul Islam <mdforhadul44@gmail.com>
2025-08-10 18:16:25 +0600

    addd allll

--- src/components/Home/HeroHomeSection.tsx
+++ src/components/Home/HeroHomeSection.tsx
@@ -1,10 +1,22 @@
+import Globe from "../ui/globe";
 import HeroHomeSectionBox from "./HeroHomeSectionBox";
 
 const HeroHomeSection = () => {
   return (
     <div className="w-full h-auto bg-1">
-      <div className="container m-auto md:h-[60vh] p-4">
-        <div className="w-full h-auto pt-20 pb-5">
+      <div className="container m-auto p-4 relative overflow-hidden">
+        
+<Globe
+  theta={0.2}
+  dark={1}
+  scale={1.2}
+  diffuse={1.5}
+  baseColor="#008000"
+  markerColor="#008000"
+  glowColor="#008000"
+  className={` container left-0 absolute -bottom-[40%] sm:-bottom-[60%] md:-bottom-[80%] lg:-bottom-[110%] xl:-bottom-[200%] m-auto`}
+/>
+        <div className="w-full h-auto pt-16 pb-5 z-[20]">
           <h1 className="text-5xl sm:text-5xl  font-extrabold text-white overflow-hidden">
             ZYPCO COURIER SOLUTATIONS
           </h1>
--- src/components/Home/HeroHomeSectionBox.tsx
+++ src/components/Home/HeroHomeSectionBox.tsx
@@ -1,68 +1,85 @@
-"use client"
-import { Calculator, PackagePlus, PackageSearch } from 'lucide-react'
-import React, { useState } from 'react'
+"use client";
+import { Calculator, PackagePlus, PackageSearch } from "lucide-react";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 const HeroHomeSectionBox = () => {
-    const [activeBox, setActiveBox] = useState({
-        calculatorBox : false,
-        trackingBox:true,
-        createBox: false
-    })
+  const [activeBox, setActiveBox] = useState({
+    calculatorBox: false,
+    trackingBox: true,
+    createBox: false,
+  });
   return (
-    
-        <div className="w-full h-auto grid lg:grid-cols-2 lg:grid-rows-1 grid-cols-1 grid-rows-2 justify-between pt-8">
-          <div className=" lg:w-[95%] bg-white rounded-2xl p-3 shadow-2xl">
-            <div className="w-full h-auto flex justify-evenly align-middle items-center">
-              <div className={`w-full h-auto p-5  ${activeBox.calculatorBox && "bg-2 text-white hover:bg-2-90"} transition-all duration-300 rounded-tl-lg cursor-pointer flex justify-center align-middle items-center `} onClick={()=>{
-                setActiveBox({
-                    trackingBox:false,
-                    createBox:false,
-                    calculatorBox:true
-                })
-              }}>
-                <Calculator  size={45}/>
-              </div>
-              <div className={`w-full p-5 ${activeBox.trackingBox && "bg-2 text-white hover:bg-2-90"} transition-all duration-300 cursor-pointer flex justify-center align-middle items-center`} 
-              onClick={()=>{
-                setActiveBox({
-                    trackingBox:true,
-                    createBox:false,
-                    calculatorBox:false
-                })
-              }}>
-                <PackageSearch size={45} />
-              </div>
-              <div className={`w-full p-5 ${activeBox.createBox && "bg-2 text-white hover:bg-2-90"} transition-all duration-300 cursor-pointer rounded-tr-lg flex justify-center align-middle items-center`} onClick={()=>{
-                setActiveBox({
-                    trackingBox:false,
-                    createBox:true,
-                    calculatorBox:false
-                })
-              }}>
-                <PackagePlus size={45} />
-              </div>
-            </div>
-            
-{
-    activeBox.calculatorBox && <div className='w-full h-auto bg-2 rounded-b-lg p-3'>
-        <div>
-            <input/>
-        </div>
-    </div>
-}
-{
-    activeBox.trackingBox && <div></div>
-}
-
-{
-    activeBox.createBox && <div></div>
-}
-
+    <div className="w-full h-auto grid lg:grid-cols-2 lg:grid-rows-1 grid-cols-1 grid-rows-2 justify-between pt-5 ">
+      <div className=" w-full lg:w-[95%] bg-white rounded-2xl p-2 shadow-2xl">
+        <div className="w-full h-auto flex justify-evenly align-middle items-center">
+          <div
+            className={`w-full h-auto p-4  ${
+              activeBox.calculatorBox && "bg-2 text-white hover:bg-2-90"
+            } transition-all duration-300 rounded-tl-lg cursor-pointer flex justify-center align-middle items-center `}
+            onClick={() => {
+              setActiveBox({
+                trackingBox: false,
+                createBox: false,
+                calculatorBox: true,
+              });
+            }}
+          >
+            <Calculator size={38} />
           </div>
-
-          <div></div>
+          <div
+            className={`w-full p-4 ${
+              activeBox.trackingBox && "bg-2 text-white hover:bg-2-90"
+            } transition-all duration-300 cursor-pointer flex justify-center align-middle items-center`}
+            onClick={() => {
+              setActiveBox({
+                trackingBox: true,
+                createBox: false,
+                calculatorBox: false,
+              });
+            }}
+          >
+            <PackageSearch size={38} />
+          </div>
+          <div
+            className={`w-full p-4 ${
+              activeBox.createBox && "bg-2 text-white hover:bg-2-90"
+            } transition-all duration-300 cursor-pointer rounded-tr-lg flex justify-center align-middle items-center`}
+            onClick={() => {
+              setActiveBox({
+                trackingBox: false,
+                createBox: true,
+                calculatorBox: false,
+              });
+            }}
+          >
+            <PackagePlus size={38} />
+          </div>
         </div>
-  )
-}
 
-export default HeroHomeSectionBox
+        {activeBox.calculatorBox && (
+          <div className="w-full h-auto bg-2 rounded-b-lg p-3">
+            <div>
+              <input />
+            </div>
+          </div>
+        )}
+        {activeBox.trackingBox && (
+          <div className="w-full h-auto bg-2 rounded-b-lg p-3 flex-col flex gap-3">
+            <Input className="bg-white text-xl md:text-xl p-4 py-6" />
+            <Button className="bg-1 text-white w-full p-6 cursor-pointer text-xl">
+              TRACK SHIPPMENT
+            </Button>
+          </div>
+        )}
+
+        {activeBox.createBox && <div></div>}
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default HeroHomeSectionBox;
