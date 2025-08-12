eda5acf3272c7a4f506c02ee5690501dec7e01fc
FOrhadul Islam <mdforhadul44@gmail.com>
2025-08-12 12:53:35 +0600

    addd allllll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroHomeSection from "@/components/Home/HeroHomeSection";
+import HomeSaliderSectation from "@/components/Home/HomeSaliderSectation";
 
 export default function Home() {
   return (
     <>
       <HeroHomeSection />
+      <HomeSaliderSectation />
     </>
   );
 }
---
+++ src/components/Home/HomeSaliderSectation.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React from 'react'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
+
+const slider_image = [
+  { id: 135, src: "/slider.jpg" },
+  { id: 2345, src: "/slider.jpg" },
+  { id: 123, src: "/slider.jpg" },
+  { id: 234, src: "/slider.jpg" },
+  { id: 156, src: "/slider.jpg" },
+  { id: 278, src: "/slider.jpg" },
+  
+  { id: 1535, src: "/slider.jpg" },
+  { id: 267, src: "/slider.jpg" },
+  { id: 165, src: "/slider.jpg" },
+  { id: 243, src: "/slider.jpg" },
+  { id: 121, src: "/slider.jpg" },
+  { id: 23456, src: "/slider.jpg" },
+
+
+
+
+];
+ 
+
+
+const HomeSaliderSectation = () => {
+  const [api, setApi] = useState<CarouselApi | null>(null);
+
+  useEffect(() => {
+    if (!api) return;
+
+    const interval = setInterval(() => {
+      api.scrollNext();
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, [api]);
+
+  return (
+    <div className="w-full h-auto py-16 pb-0 bg-white px-5">
+      <div className="container h-auto m-auto p-10">
+        <Carousel setApi={setApi}>
+          <CarouselContent>
+            {slider_image.map((item) => (
+              <CarouselItem key={item.id}>
+                <div
+                  className="w-full h-[230px] md:h-[480px] p-2 bg-cover rounded-lg bg-no-repeat bg-center"
+                  style={{ backgroundImage: `url(${item.src})` }}
+                ></div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default HomeSaliderSectation
