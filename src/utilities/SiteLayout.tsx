915fbc72499d55838cde9492d7f23314ec644896
Forhadul Islam <mdforhadul44@gmail.com>
2025-09-16 02:14:32 +0600

    add allll

--- src/utilities/SiteLayout.tsx
+++ src/utilities/SiteLayout.tsx
@@ -1,8 +1,23 @@
 "use client";
 import FooterBar from "@/components/Footer/FooterBar";
+import { AppSideBar } from "@/components/Nav/AppSideBar";
 import NavBar from "@/components/Nav/NavBar";
-import SaidBar from "@/components/Nav/SaidBar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { useAuth } from "@/hooks/AuthContext";
+import { BellRing } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { Toaster } from "sonner";
@@ -12,6 +27,7 @@ export default function SiteLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathNameArray = usePathname().split("/");
   const pathName = usePathname().split("/")[1];
   const router = useRouter();
   const auth = useAuth();
@@ -23,22 +39,68 @@ export default function SiteLayout({
   if (pathName == "dashboard" && auth.user?.token) {
     return (
       <>
-        <SaidBar />
-        {children}
-        <Toaster expand={false} position="top-center" />
+        <SidebarProvider>
+          <AppSideBar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+              <div className="flex items-center gap-2 px-4 relative w-full">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    {pathNameArray.map((item) => (
+                      <div key={item}>
+                        <BreadcrumbItem className="hidden md:block">
+                          <BreadcrumbLink
+                            href={`/${pathName}`}
+                            className=" capitalize"
+                          >
+                            {item}
+                          </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        {pathNameArray.length >= 3 ? (
+                          <BreadcrumbSeparator className="hidden md:block" />
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    ))}
+                  </BreadcrumbList>
+                </Breadcrumb>
+
+                <div className=" absolute right-5 top-[8%] flex justify-center align-middle items-center gap-5">
+                  <BellRing size={24} />
+                  <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={"/"} alt={"USER PROFILE"} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                </div>
+
+              </div>
+            </header>
+            <div>
+              {children}
+
+              <Toaster expand={false} position="top-center" />
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
       </>
     );
   }
 
-  
-
-  return (
-    <>
-      <NavBar />
-      <div className="w-full h-[85px]"></div>
-      {children}
-      <FooterBar />
-      <Toaster expand={false} position="top-center" />
-    </>
-  );
+  if (pathName != "dashboard") {
+    return (
+      <>
+        <NavBar />
+        <div className="w-full h-[85px]"></div>
+        {children}
+        <FooterBar />
+        <Toaster expand={false} position="top-center" />
+      </>
+    );
+  }
 }
